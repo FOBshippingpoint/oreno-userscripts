714403ed00931caae3710088286c89ca446778a7
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2022-07-11 22:04:37 +0800

    refactor(youtube_no_shorts.user.js): change icon resource url

--- youtube-no-shorts/youtube_no_shorts.user.js
+++ youtube-no-shorts/youtube_no_shorts.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name            YouTube No Shorts
 // @author          FOBshippingpoint
-// @namespace       http://www.example.url/to/your-web-site/
+// @namespace       https://github.com/FOBshippingpoint/oreno-userscripts
 // @description     Redirects to the original UI of the YouTube video. (Shorts is bad, you can't even pause the video!)
 // @license         MIT
 // @version	        0.1
-// @icon            https://i.imgur.com/Td5OXvR.png
+// @icon            https://raw.githubusercontent.com/FOBshippingpoint/oreno-userscripts/main/youtube-no-shorts/icon.png
 // @include         https://www.youtube.com/shorts/*
 // @include         http://www.youtube.com/shorts/*
 // ==/UserScript==
