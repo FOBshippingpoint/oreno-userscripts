9dc23ceac3830d9a709eb3090d9817533f9bbc2b
FOBshippingpoint <aulaulql4ql4xj6@gmail.com>
2022-07-12 17:51:28 +0800

    docs(youtube_no_shorts.user.js): add some url for script, and add zh-Tw description

--- youtube-no-shorts/youtube_no_shorts.user.js
+++ youtube-no-shorts/youtube_no_shorts.user.js
@@ -1,14 +1,18 @@
 // ==UserScript==
-// @name            YouTube No Shorts
-// @author          FOBshippingpoint
-// @namespace       https://github.com/FOBshippingpoint/oreno-userscripts
-// @description     Redirects to the original UI of the YouTube video. (Shorts is bad, you can't even pause the video!)
-// @license         MIT
-// @version	        0.1
-// @icon            https://raw.githubusercontent.com/FOBshippingpoint/oreno-userscripts/main/youtube-no-shorts/icon.png
-// @include         https://www.youtube.com/shorts/*
-// @include         http://www.youtube.com/shorts/*
-// @updateURL       https://raw.githubusercontent.com/FOBshippingpoint/oreno-userscripts/main/youtube-no-shorts/youtube_no_shorts.user.js
+// @name                  YouTube No Shorts
+// @name:zh-TW            我不要YouTube Shorts
+// @author                FOBshippingpoint
+// @namespace             https://github.com/FOBshippingpoint/oreno-userscripts
+// @description           Redirects to the original UI of the YouTube video. (Shorts is bad, you can't even change progress!)
+// @description:zh-TW     重新導向至YouTube經典影片播放界面（而不是連調整進度都做不到的Shorts界面）
+// @license               MIT
+// @version               0.1.1
+// @icon                  https://raw.githubusercontent.com/FOBshippingpoint/oreno-userscripts/main/youtube-no-shorts/icon.png
+// @include               https://www.youtube.com/shorts/*
+// @include               http://www.youtube.com/shorts/*
+// @updateURL             https://raw.githubusercontent.com/FOBshippingpoint/oreno-userscripts/main/youtube-no-shorts/youtube_no_shorts.user.js
+// @supportURL            https://github.com/FOBshippingpoint/oreno-userscripts/issues
+// @homepageURL           https://github.com/FOBshippingpoint/oreno-userscripts#readme
 // ==/UserScript==
 
 window.location = document.URL.replace('shorts/', 'watch?v=');
