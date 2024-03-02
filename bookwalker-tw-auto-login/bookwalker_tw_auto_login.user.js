9769d9bee7b7604dfb9b1360aa00a34644ff43b9
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-03-03 03:32:12 +0800

    fix: update css selector for login status check

--- bookwalker-tw-auto-login/bookwalker_tw_auto_login.user.js
+++ bookwalker-tw-auto-login/bookwalker_tw_auto_login.user.js
@@ -5,7 +5,7 @@
 // @description           Auto login to your bookwalker account.
 // @license               MIT
 // @version               1.0
-// @icon                  https://raw.githubusercontent.com/FOBshippingpoint/oreno-userscripts/main/bookwalker-tw-auto-login/icon.png
+// @icon                  https://raw.githubusercontent.com/FOBshippingpoint/oreno-userscripts/main/bookwalker-tw-auto-login/icon.svg
 // @include               https://github.com
 // @updateURL             https://raw.githubusercontent.com/FOBshippingpoint/oreno-userscripts/main/bookwalker-tw-auto-login/bookwalker_tw_auto_login.user.js
 // @supportURL            https://github.com/FOBshippingpoint/oreno-userscripts/issues
@@ -13,8 +13,11 @@
 // @match                 https://member.bookwalker.com.tw/login
 // @match                 https://www.bookwalker.com.tw/*
 // @grant                 GM_info
-// @require               https://code.jquery.com/jquery-3.6.4.slim.min.js
 // ==/UserScript==
+
+// very simple jQuery
+const $ = (s) => document.querySelector(s);
+const $$ = (s) => [...document.querySelectorAll(s)];
 
 // 👇👇👇 Please fill in your own account infomation.
 const ACCOUNT = {
@@ -23,18 +26,18 @@ const ACCOUNT = {
 };
 
 // Check if the user really set their own account infomation
-if (ACCOUNT.email === "Not set" || ACCOUNT.password === "Not set") {
+if (ACCOUNT.email == "Not set" || ACCOUNT.password == "Not set") {
   alert(
     "Please set your own email and password in `Bookwalker TW Auto Login` userscript",
   );
 } else {
   // If the login button is not exist, redirect uesr to the login page
-  const loginBtnExist = $(".topMemberItem.pcLogin").length;
+  const loginBtnExist = $(".topLoginStyle") != null;
   if (loginBtnExist) {
     window.location = "https://member.bookwalker.com.tw/login";
   }
 
-  $("#email").val(ACCOUNT.email);
-  $("#password").val(ACCOUNT.password);
+  $("#email").value = ACCOUNT.email;
+  $("#password").value = ACCOUNT.password;
   $("#login").click();
 }
